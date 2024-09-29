f02d255ad7c81aa005bdfde2b7439bf4fd18cf11
maxwel kirimi <maxwelkirimii@gmail.com>
2024-09-29 14:24:57 +0300

    document structure

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob'
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName=
+    customerName.toUpperCase();
+
+}
+function setBestCustomer() {
+    bestCustomer='not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer='maybe bob';
+} 
+const leastFavoriteCustomer= 'name'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer= 'not name';
+}
